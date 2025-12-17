7c187b6151a2d1c8e37409b20d685729956f3fd6
jungbinl <jbiny1004@naver.com>
2025-12-16 17:27:49 -0700

    Define displayInfo() for Undergrad class

--- project4-database-C++/undergrad.cpp
+++ project4-database-C++/undergrad.cpp
@@ -1,7 +1,4 @@
 
-// Q2a: Define displayInfo() (5 points)
-// Define the fucntion displayInfo() for Undergrad class
-// See expected output in question file.
 #include <iostream>
 #include "undergrad.h"
 using namespace std;
@@ -13,5 +10,5 @@ void Undergrad::displayInfo() {
 	cout << "Roll number: " << Student::getRollNo() << endl;
 
 }
-// (displayList() function in main.cpp can call this function.)
-// Include necessary header files
+
+
