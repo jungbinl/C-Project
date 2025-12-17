6a9b6ab80a48d90ccc7e123e73f9c43b611bfc63
jungbinl <jbiny1004@naver.com>
2025-12-16 17:23:48 -0700

    Update p01.c

--- project1-basic string num/p01.c
+++ project1-basic string num/p01.c
@@ -1,13 +1,8 @@
-// CSE220 Spring 2025, Project 3
-
-// Your name here
-// Programming environment used: VS or gcc
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#pragma warning(disable : 4996)  // to avoid scanf error, Visual Studio only
+#pragma warning(disable : 4996) 
 
 #define MAX_DIM 3
 #define STUDENTS 5		// total number of students
@@ -15,10 +10,6 @@ int matrixA[MAX_DIM][MAX_DIM], matrixB[MAX_DIM][MAX_DIM], sum[MAX_DIM][MAX_DIM];
 int scores[STUDENTS];
 int *pScores = NULL;
 
-/*
-Question 1: Forward declaration (5 points)
-Declare here the functions used in the program, as needed
-*/
 // all void function no return
 void inputMatrixA();
 void copyToMatrixB();
@@ -62,15 +53,6 @@ int main()
 	return 0;
 }
 
-
-
-
-/*
-Question 2: inputMatrixA (5 points)
-matrixA[][] is already declared. Input the elements of matrixA from the user.
-Use the macro MAX_DIM in your code, so that when MAX_DIM is changed from 3 to any other number, this function would still work.
-Note: Usually, matrix is entered row-wise. So input 1st row of elemets , followed by 2nd row and so on.
-*/
 void inputMatrixA()
 {
 	for(int i = 0; i < MAX_DIM; i++){
@@ -83,15 +65,6 @@ void inputMatrixA()
 	}
 	      			      
 }
-/*
-
-Question 3: copyToMatrixB (5 points)
-matrixB[][] is already declared. Compose matrixB identical to matrixA.
-Copy elements of matrixA to matrixB. You may use array operation or pointer to do the copying.
-So, after calling this function, matrixB and matrixA should have same elements.
-Use the macro MAX_DIM in your code, so that when MAX_DIM is changed from 3 to any other number, this function would still work.
-Debug tip: Use display function below to temporarily display matrixA and matrixB to verify if your functions work correctly.
-*/
 
 void copyToMatrixB()
 {
@@ -102,13 +75,6 @@ void copyToMatrixB()
 	}
 }
 
-
-/*
-Question 4: addMatrix (10 points)
-sum[][] is already declared. Implement this math operation: (2*A) + B and store the result in matrix 'sum'.
-Use the macro MAX_DIM in your code, so that when MAX_DIM is changed from 3 to any other number, this function would still work.
-*/
-
 void addMatrix()
 {
 	for(int i = 0; i < MAX_DIM; i++){
@@ -117,15 +83,6 @@ void addMatrix()
 		}
 	}
 }
-
-/*
-Question 5: displaySumMatrix (10 points)
-Implement the function to display the 'sum' matrix. Display in this format :
-The matrix sum=
-1	2	3
-4	5	6
-7	8	9
-*/
 
 void displaySumMatrix()
 {
@@ -141,14 +98,6 @@ void displaySumMatrix()
 	}
 }
 
-/*
-Question 6: inputScores (10 points)
-Input the scores of students.
-The valid range of score is 0 to 100 (0 and 100 included). Keep prompting the user to input valid score if invalid score is input.
-This function should work as expected when STUDENTS macro is changed.
-You MUST use pointer 'pScores' to input the scores to gain full points. 
-Using array operation (scores[i]) will earn you half points.
-*/
 void inputScores()
 {
 	pScores = &scores[0];
@@ -166,19 +115,8 @@ void inputScores()
 		}while(temp <0 || temp >100); // if temp is more than 100, less than 0, keep scan
 		
 	}		
-	
-
-
 }
 
-/*
-Question 7: scoreStats (10 points)
-Now that the scores of students are stored, find the maximun and the average of those scores. Display these stats.
-Ignore the decimal point accuracy since we are dealing with integers. This function should work as expected when STUDENTS macro is changed.
-You MUST use pointer 'pScores' to gain full points.
-Using array operation (scores[i]) will earn you half points.
-Note: Make sure pointer 'pScores' is pointing to correct place.
-*/
 void scoreStats()
 {
 int a = 0;
@@ -192,18 +130,8 @@ for(int i = 0; i < STUDENTS; i++){
 int avg = a / STUDENTS; // sum divided by students, get avg
 printf("\nMax score= %d\n", max);
 printf("Avg score= %d\n", avg);
-
-
 }
 
-/*
-Question 8: createUsername (15 points)
-Implement the function that creates and displays the user name of the user. Ask the user for their first and last name.
-The username is the first letter of first name, followed by the last name. For instance, if the user's name is John Doe, then the username is jdoe.
-The user may input upper or lower case characters for first and last name. Make sure that the username is all UPPER CASE.
-Note : When using fgets(), when you press Enter to finish entering the string, the Enter (\n) character is also added to the string.
-	Try to remove that character.
-*/
 void createUsername()
 {
 	char firstName[15], lastName[15], userName[20];
@@ -226,14 +154,6 @@ void createUsername()
 	printf("User name : %s", userName );
 }
 
-/*
-Question 9: (10 points)
-Take in a N-character string (max = 128)'sentence' then count the number of lower case characters in array 'sentence' and afterwards convert lower case characters to upper case. see the sample output in PDF file.
-Print the number of lower case characters and print the newly formed sentence[] which has all upper case characters.
-You may not add more variables than already declared. You may use 'int i' in 'for' loop, if needed.
-Hint: Lookup ASCII chart of alphabets
-*/
-
 void lowerToUpperCase()
 {
 	unsigned int len_max = 128;
@@ -250,23 +170,9 @@ void lowerToUpperCase()
 			sentence[i] = toupper(sentence[i]); // Convert lowercase to uppercase
 		}
 	}
-
-
-
 	printf("Number of lower case characters= %d \n", lowerCases);
 	printf("Upper case sentence : %s \n",sentence);
 }
-
-/*
-Question 10: (20 points)
-Implement the function to take in a N-character string (max = 128)'string'1' and 3-character pattern string 'pattern' from the user.
-Search for the 'pattern' in 'str' and display "Pattern found" if 'pattern' is found in 'str' also return the locations of the three indexes, else display "Pattern not found".
-For instance, if user enters str= "abcdefghij" and pattern = "ghi", then the pattern exists in str at locations 6,7,8 (print these) and display "Pattern found". The start point for location searching is from zero.
-You MUST use pointers and not arrays for this question. You may use string functions.
-You may not add more variables than already declared. You may use 'int i' in 'for' loop, if needed.
-
-Note : The user input of the string is not predefined and it can be of any length unless user use enter or /n. so try to use realloc here.
-*/
 
 void searchPattern()
 {
@@ -307,4 +213,5 @@ void searchPattern()
 	free(str);
 	free(pattern);
 }
+
 
