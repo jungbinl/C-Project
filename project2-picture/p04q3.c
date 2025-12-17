ce89c37b179f72e20ab2927aa0e7a97d62506bc6
jungbinl <jbiny1004@naver.com>
2025-12-16 17:24:48 -0700

    Refactor file operations and word counting logic

--- project2-picture/p04q3.c
+++ project2-picture/p04q3.c
@@ -1,9 +1,3 @@
-// CSE 220 Spring 2025
-// Project 4
-
-// Enter your name here
-// Mention which environment you used: VS or gcc
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,13 +9,6 @@
 
 FILE *fp;		// Use this file pointer to open the file in readFile() and use this pointer through out this file.
 
-/*
-Problem 1: (10 points)
-Open the file proj4.txt. Use proper parameters to open the file for reading as well as writing.
-After opening the file, you will use the 'fp' pointer in other functions.
-Display the size of the file.
-Note: If using VS and default project location then place proj4.txt here- "C:\Users\<user>\Documents\Visual Studio 2017\Projects\Project1\Project1"
-*/
 void readFile()
 {
 	int fileSize = 0;
@@ -33,11 +20,6 @@ void readFile()
 	printf("The file size = %d bytes\n", fileSize);
 }
 
-/*
-Problem 2: (8 points)
-Using file operations, calculate and display the number of words in the .txt file.
-You are NOT allowed to use string library functions.
-*/
 void wordCount()
 {
 	int words = 0;
@@ -60,11 +42,6 @@ prev = c;
 	printf("Total words = %d \n", words);
 }
 
-/*
-Problem 3: (7 points)
-Using file operations, calculate and display the number of sentences in the .txt file.
-You are NOT allowed to use string library functions.
-*/
 void sentenceCount()
 {
 	int sentences = 0;
@@ -80,14 +57,6 @@ void sentenceCount()
 	printf("Total sentences = %d \n", sentences);
 }
 
-/*
-Problem 4: (15 points)
-In findAndReplace(), ask the user for a word (find[]) to replace and the new word (replace[]) that will be inserted in its place.
-For simplicity, you may assume that both these words will be of same length and replace the first occurance of the 'find' word.
-If the word is found and replaced, then print "Found and replaced", lese print "Word not found"
-For example, user might enter find[]= "with" and replace[]= "into". So all instances of the word "with" should be replaced by "into".
-You are allowed to use only these string functions here, if necessary: strlen(), strcmp(), strcat().
-*/
 void findAndReplace()
 {
 	char find[WORD_LENGTH], replace[WORD_LENGTH];
@@ -145,3 +114,4 @@ int main()
 	fclose(fp);
 	return 0;
 }
+
