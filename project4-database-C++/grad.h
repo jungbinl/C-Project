3a7a0d1c90a97f5fc079bfc80f315d53d3a4aa7d
jungbinl <jbiny1004@naver.com>
2025-12-16 17:26:22 -0700

    Refactor grad.h header guard and formatting

--- project4-database-C++/grad.h
+++ project4-database-C++/grad.h
@@ -2,8 +2,6 @@
 #define _GRAD_H_
 #include <string>
 #include "student.h"
-// Q1b: Create Grad class (5 points)
-// Part 1: Create a child class of the Student class named 'Grad'
 using namespace std;
 
 class Grad : public Student {
@@ -12,10 +10,5 @@ public:
 	Grad(string, int, studentType);
 	virtual void displayInfo() override;
 };
-
-// Part2: Declare constructor which accepts the same 3 parameters as the parent class Student's constructor.
-// Pass the 3 parameters to the super constructor of the Student class.
-
-// Part 3: Re-declare the method displayInfo (virtual method found inside of parent class Student)
 
 #endif // _GRAD_H_
